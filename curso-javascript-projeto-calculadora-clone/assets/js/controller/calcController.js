585a58ae995d5eb9ab8fb93548883b35d4a39fa2
Patrick Vieira Feitosa <patrick.feitosa4@etec.sp.gov.br>
2018-08-05 01:51:28 -0300

    Implementado atualizador de Hora e Data no Display da calculadora

--- curso-javascript-projeto-calculadora-clone/assets/js/controller/calcController.js
+++ curso-javascript-projeto-calculadora-clone/assets/js/controller/calcController.js
@@ -1,19 +1,59 @@
-class CalcController{
-    constructor(){
-        this._displayCalc = '0';
+class CalcController {
+    /** 
+     * The constructor for the CalcController Class
+    */
+    constructor() {
+        this._locale = 'pt-BR';
         this._currentDate;
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+        this.initialize();
     }
 
-    get displayCalc(){
-        return this._displayCalc;
+    /** 
+     * Method that initialize the class
+    */
+    initialize() {
+        this.setDisplayDateTime()
+        setInterval(() => {
+            this.setDisplayDateTime()
+        }, 1000);
     }
-    set displayCalc(displayCalc){
-        this._displayCalc = displayCalc;
+
+    /** 
+     * Method to update the Current Time and Date
+    */
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
     }
-    get currentDate(){
-        return this._currentDate;
+
+    /**
+     * Getters and Setters for the Class
+     */
+    get displayDate() {
+        return this._dateEl.innerHTML;
     }
-    set currentDate(currentDate){
-        this._currentDate = currentDate;
+
+    set displayDate(displayDate) {
+        return this._dateEl.innerHTML = displayDate;
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(displayTime) {
+        return this._timeEl.innerHTML = displayTime
+    }
+    get displayCalc() {
+        return this._displayCalcEl.innerHTML;
+    }
+    set displayCalc(displayCalc) {
+        this._displayCalcEl.innerHTML = displayCalc;
+    }
+    get currentDate() {
+        return new Date();
     }
 }
