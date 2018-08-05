7b2f817c37c41869f93a5944fccd2dfab0d61128
Patrick Vieira Feitosa <patrick.feitosa4@etec.sp.gov.br>
2018-08-05 02:45:26 -0300

    Implementado metodos para adição de eventos a um elemento

--- curso-javascript-projeto-calculadora-clone/assets/js/controller/calcController.js
+++ curso-javascript-projeto-calculadora-clone/assets/js/controller/calcController.js
@@ -9,6 +9,7 @@ class CalcController {
         this._dateEl = document.querySelector('#data');
         this._timeEl = document.querySelector('#hora');
         this.initialize();
+        this.initButtonEvents();
     }
 
     /** 
@@ -21,6 +22,33 @@ class CalcController {
         }, 1000);
     }
 
+    /**
+     * 
+     * @param {HTMLElement} element element in the DOM that will added the function
+     * @param {String} events string with all the events that need the execute the same function
+     * @param {Function} fn function that will be executed at the element at all events listed 
+     */
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn);
+        })
+    }
+
+    /** 
+     * Method to initialize all the events in the calculator
+    */
+    initButtonEvents() {
+        let allButtons = document.querySelectorAll('#buttons > g, #parts > g');
+        Array.from(allButtons).forEach((btn, index) => {
+            this.addEventListenerAll(btn, 'click drag', e => {
+                console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+                btn.style.cursor = 'pointer';
+            });
+        })
+    }
     /** 
      * Method to update the Current Time and Date
     */
